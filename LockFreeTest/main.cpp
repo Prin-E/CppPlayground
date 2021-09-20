7f8a3e89f6d6c303b7c7bf0dfd41d9749b38a15e
Prin_E <ddderer@gmail.com>
2021-09-20 22:57:22 +0900

    Commit lock free test
    initial commit :+1:

---
+++ LockFreeTest/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <atomic>
+#include <thread>
+
+// spinlock mutex using atomic flag
+class spinlock_mutex {
+public:
+    void lock() {
+        while(flag.test_and_set(std::memory_order_acquire));
+    }
+    
+    void unlock() {
+        flag.clear(std::memory_order_release);
+    }
+    
+private:
+    std::atomic_flag flag = ATOMIC_FLAG_INIT;
+};
+
+// global variables
+spinlock_mutex global_mutex;
+std::atomic<int> global_intval{0};
+
+// thread main
+void atomic_flag_thread_main() {
+    global_mutex.lock();
+    int val = global_intval.fetch_add(1, std::memory_order_relaxed) + 1;
+    std::cout << "Called from thread! (counter:" << val << ")" << std::endl;
+    global_mutex.unlock();
+}
+
+int main(int argc, const char * argv[]) {
+    // test flags
+    constexpr bool test_atomic_flag = true;
+    
+    // atomic_flag
+    if(test_atomic_flag)
+    {
+        spinlock_mutex mutex;
+        mutex.lock();
+        mutex.unlock();
+        
+        // duplicated lock (deadlock)
+        /*
+        mutex.lock();
+        mutex.lock();
+        mutex.unlock();
+        mutex.unlock();
+         */
+        
+        // multiple threads
+        constexpr int num_threads = 8;
+        constexpr int num_iteration = 10;
+        std::cout << "Running " << num_threads << " threads... (iteration:" << num_iteration << ")" << std::endl;
+        for(int i = 0; i < num_iteration; i++) {
+            std::cout << "--------------------------------" << std::endl;
+            std::cout << "- Iteration " << (i + 1) << std::endl;
+            std::thread ts[num_threads];
+            for(int k = 0; k < num_threads; k++) {
+                ts[k] = std::thread(atomic_flag_thread_main);
+            }
+            for(int k = 0; k < num_threads; k++) {
+                ts[k].join();
+            }
+        }
+        std::cout << "--------------------------------" << std::endl;
+        std::cout << "Complete!" << std::endl;
+    }
+    
+    return 0;
+}

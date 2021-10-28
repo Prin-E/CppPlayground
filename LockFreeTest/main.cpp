90ef9109f1f454ca4c80c1c000eabb3e8a234d96
Prin_E <ddderer@gmail.com>
2021-10-29 01:27:39 +0900

    Implementing lock free stack and test code - still has ABA problems yet...

--- LockFreeTest/main.cpp
+++ LockFreeTest/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <thread>
+#include <vector>
+#include <algorithm>
 #include "../Shared/Shared.h"
 
 // global variables
@@ -14,13 +16,42 @@ void atomic_flag_thread_main() {
     global_mutex.unlock();
 }
 
+void lock_free_stack_thread_push_main(lf_default_stack *stack, int thread_index, int num_iteration, int *value_log) {
+    for(int i = 0; i < num_iteration; i++) {
+        int value = thread_index * num_iteration + i;
+        stack->push(value);
+        value_log[i] = value;
+    }
+}
+
+void lock_free_stack_thread_pop_main(lf_default_stack *stack, int thread_index, int num_iteration, int *value_log) {
+    int value = 0;
+    for(int i = 0; i < num_iteration; i++) {
+        while(!stack->pop(value));
+        value_log[i] = value;
+    }
+}
+
+void lock_free_stack_thread_push_stress_main(lf_default_stack *stack, int thread_index, int num_iteration) {
+    for(int i = 0; i < num_iteration; i++) {
+        stack->push(rand()%10000);
+    }
+}
+
+void lock_free_stack_thread_pop_stress_main(lf_default_stack *stack, int thread_index, int num_iteration) {
+    int value = 0;
+    for(int i = 0; i < num_iteration; i++) {
+        while(!stack->pop(value));
+    }
+}
+
 int main(int argc, const char * argv[]) {
     // test flags
-    constexpr bool test_atomic_flag = true;
+    constexpr bool test_atomic_flag = false;
+    constexpr bool test_lf_stack = true;
     
     // atomic_flag
-    if(test_atomic_flag)
-    {
+    if(test_atomic_flag) {
         spinlock_mutex mutex;
         mutex.lock();
         mutex.unlock();
@@ -50,6 +81,170 @@ int main(int argc, const char * argv[]) {
         }
         std::cout << "--------------------------------" << std::endl;
         std::cout << "Complete!" << std::endl;
+    }
+    
+    if(test_lf_stack) {
+        lf_default_stack stack;
+        
+        // single threaded test
+        {
+            std::cout << "Single-threaded push and pop test..." << std::endl;
+            
+            // push
+            stack.push(3);
+            stack.push(4);
+            stack.push(2);
+            
+            // pop
+            int val = 0;
+            while(stack.pop(val))
+                std::cout << val << std::endl;
+            
+            std::cout << "--------------------------------" << std::endl;
+            std::cout << "Complete!" << std::endl << std::endl;
+        }
+        
+        // multi threaded test (push validation)
+        {
+            std::cout << "Multi-threaded push test..." << std::endl;
+            constexpr int num_push_threads = 16;
+            constexpr int num_push_iteration = 1000;
+            int push_value_log[num_push_threads][num_push_iteration];
+            int stack_values[num_push_threads * num_push_iteration];
+            uintptr_t stack_counters[num_push_threads * num_push_iteration];
+            
+            std::cout << "Running " << num_push_threads << " threads... (iteration:" << num_push_iteration << ")" << std::endl;
+            
+            std::thread ts_push[num_push_threads];
+            for(int k = 0; k < num_push_threads; k++) {
+                ts_push[k] = std::thread(lock_free_stack_thread_push_main, &stack, k, num_push_iteration, push_value_log[k]);
+            }
+            for(int k = 0; k < num_push_threads; k++) {
+                ts_push[k].join();
+            }
+            std::cout << "--------------------------------" << std::endl;
+            
+            // validate pushed values
+            std::vector<int> push_vec, stack_values_vec;
+            std::vector<uintptr_t> stack_counters_vec;
+            for(int i = 0; i < num_push_threads; i++) {
+                push_vec.insert(push_vec.end(), std::begin(push_value_log[0]), std::end(push_value_log[0]));
+            }
+            stack.debug_fetch(stack_values, stack_counters, num_push_threads * num_push_iteration);
+            stack_values_vec.insert(stack_values_vec.end(), std::begin(stack_values), std::end(stack_values));
+            stack_counters_vec.insert(stack_counters_vec.end(), std::begin(stack_counters), std::end(stack_counters));
+            std::sort(stack_values_vec.begin(), stack_values_vec.end());
+            std::sort(stack_counters_vec.begin(), stack_counters_vec.end());
+
+            int validation_value = -1;
+            bool validation_flag = true;
+            for(size_t i = 0; i < stack_values_vec.size(); i++) {
+                int value = stack_values_vec.at(i);
+                if(validation_value + 1 != value) {
+                    validation_flag = false;
+                    break;
+                }
+                validation_value = value;
+            }
+            
+            if(validation_flag)
+                std::cout << "Validation success! --- " << std::endl;
+            else
+                std::cout << "Validation failed! --- " << std::endl;
+            
+            std::cout << "Complete!" << std::endl << std::endl;
+        }
+        
+        // multi threaded test (push-pop validation)
+        {
+            std::cout << "Multi-threaded push and pop test..." << std::endl;
+            constexpr int num_push_threads = 1000;
+            constexpr int num_pop_threads = 1000;
+            constexpr int num_push_iteration = 100000;
+            constexpr int num_pop_iteration = 100000;
+            static_assert(num_push_threads * num_push_iteration == num_pop_threads * num_pop_iteration, "push and pop count are mismatch!");
+            
+            int *push_value_log[num_push_threads];
+            int *pop_value_log[num_pop_threads];
+            for(int i = 0; i < num_push_threads; i++) {
+                push_value_log[i] = new int[num_push_iteration];
+            }
+            for(int i = 0; i < num_pop_threads; i++) {
+                pop_value_log[i] = new int[num_pop_iteration];
+            }
+            
+            std::cout << "Running " << (num_push_threads + num_pop_threads) << " threads... (iteration:" << num_push_iteration << "," << num_pop_iteration << ")" << std::endl;
+            
+            std::thread ts_push[num_push_threads];
+            std::thread ts_pop[num_pop_threads];
+            for(int k = 0; k < num_push_threads; k++) {
+                ts_push[k] = std::thread(lock_free_stack_thread_push_main, &stack, k, num_push_iteration, push_value_log[k]);
+            }
+            for(int k = 0; k < num_pop_threads; k++) {
+                ts_pop[k] = std::thread(lock_free_stack_thread_pop_main, &stack, k, num_pop_iteration, pop_value_log[k]);
+            }
+            for(int k = 0; k < num_push_threads; k++) {
+                ts_push[k].join();
+            }
+            for(int k = 0; k < num_pop_threads; k++) {
+                ts_pop[k].join();
+            }
+            std::cout << "--------------------------------" << std::endl;
+            
+            // validate push-pop values
+            std::vector<int> push_vec, pop_vec;
+            for(int i = 0; i < num_push_threads; i++) {
+                for(int j = 0; j < num_push_iteration; j++)
+                    push_vec.push_back(push_value_log[i][j]);
+            }
+            for(int i = 0; i < num_pop_threads; i++) {
+                for(int j = 0; j < num_pop_iteration; j++)
+                    pop_vec.push_back(pop_value_log[i][j]);
+            }
+            //std::sort(push_vec.begin(), push_vec.end());
+            std::sort(pop_vec.begin(), pop_vec.end());
+            
+            if(push_vec == pop_vec) {
+                std::cout << "Validation success! --- " << std::endl;
+            }
+            else {
+                std::cout << "Validation failed! --- " << std::endl;
+            }
+            
+            for(int i = 0; i < num_push_threads; i++) {
+                delete push_value_log[i];
+            }
+            for(int i = 0; i < num_pop_threads; i++) {
+                delete pop_value_log[i];
+            }
+            std::cout << "Complete!" << std::endl << std::endl;
+        }
+        
+        // multi threaded test (stress)
+        {
+            std::cout << "Multi-threaded stress test..." << std::endl;
+            constexpr int num_push_threads = 250;
+            constexpr int num_pop_threads = 250;
+            constexpr int num_iteration = 10000000;
+            std::cout << "Running " << (num_push_threads + num_pop_threads) << " threads... (iteration:" << num_iteration << ")" << std::endl;
+            
+            std::thread ts_push[num_push_threads];
+            std::thread ts_pop[num_pop_threads];
+            for(int k = 0; k < num_push_threads; k++) {
+                ts_push[k] = std::thread(lock_free_stack_thread_push_stress_main, &stack, k, num_iteration);
+            }
+            for(int k = 0; k < num_pop_threads; k++) {
+                ts_pop[k] = std::thread(lock_free_stack_thread_pop_stress_main, &stack, k, num_iteration);
+            }
+            for(int k = 0; k < num_push_threads; k++) {
+                ts_push[k].join();
+            }
+            for(int k = 0; k < num_pop_threads; k++) {
+                ts_pop[k].join();
+            }
+            std::cout << "--------------------------------" << std::endl;
+            std::cout << "Complete!" << std::endl << std::endl;
+        }
     }
     
     return 0;
--- Shared/LockFree/LockFreeStack.h
+++ Shared/LockFree/LockFreeStack.h
@@ -15,7 +15,28 @@
 // Linked list node
 template<typename T>
 struct linked_list_node {
-    struct linked_list_node *next;
+    // node link structure
+    struct node_link {
+        union {
+            uintptr_t value;
+            struct {
+                // assumes that the 64-bit system has 52-bit or lower address space.
+                uintptr_t ptr : 52;
+                // counter variable to prevent ABA problem (0~4096)
+                uintptr_t counter : 12;
+            };
+        };
+        node_link() : value(0) {}
+        node_link(struct linked_list_node* node) : ptr((uintptr_t)node) { counter += 1; }
+        operator struct linked_list_node*() const {
+            return (linked_list_node*)ptr;
+        }
+    };
+
+    static_assert(sizeof(node_link) == sizeof(uintptr_t), "Invalid data size of node_link!");
+
+    node_link next;
+    //struct linked_list_node *next;
     T value;
     
     linked_list_node() {}
@@ -28,36 +49,64 @@ template<typename T>
 class lf_stack {
 public:
     using node_t = linked_list_node<T>;
+    using node_link_t = typename linked_list_node<T>::node_link;
     
     // constructor
     lf_stack() : head(nullptr) {}
     
     // destructor
     ~lf_stack() {
+        T val;
+        while(pop(val));
     }
     
     void push(const T &value) {
         node_t *n = new node_t(value);
+        node_link_t link;
+        link.ptr = (uintptr_t)n;
         do {
             n->next = head.load(std::memory_order_relaxed);
+            link.counter = n->next.counter + 1;
         }
-        while(!head.compare_exchange_strong(&n->next, n));
+        while(!head.compare_exchange_strong(n->next, link));
     }
 
-    T pop() {
-        node_t *n = nullptr;
+    bool pop(T &out_value) {
+        node_link_t link, next_link;
+        node_t *node = nullptr;
         do {
-            n = head.load(std::memory_order_relaxed);
+            link = head.load();
+            node = (node_t*)link.ptr;
+            std::atomic_thread_fence(std::memory_order_seq_cst);
+            if(node == nullptr)
+                break;
+            next_link = node->next;
+            next_link.counter = link.counter + 1;
         }
-        while(n != nullptr && head.compare_exchange_strong(&n, n->next));
+        while(!head.compare_exchange_weak(link, next_link));
         
-        T v = std::move(n->value);
-        delete n;
-        return v;
+        bool flag = node != nullptr;
+        if(flag) {
+            out_value = std::move(node->value);
+            delete node;
+        }
+        return flag;
+    }
+    
+    // debug-only fetch function (not thread-safe)
+    void debug_fetch(T *values, uintptr_t *counters, size_t length) {
+        node_link_t link = head.load(std::memory_order_seq_cst);
+        int index = 0;
+        while(link.ptr != 0) {
+            values[index] = ((node_t*)link.ptr)->value;
+            counters[index] = link.counter;
+            index += 1;
+            link = ((node_t*)link.ptr)->next;
+        }
     }
     
 private:
-    std::atomic<node_t*> head;
+    std::atomic<node_link_t> head;
 };
 
 // default types

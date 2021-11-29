c9c64f80e54d16d46faa02fa36edba3dafefa55c
Prin_E <ddderer@gmail.com>
2021-11-30 00:11:41 +0900

    Applied x64 BTS assembly in GCC/Clang (12~20% faster than XCHG!)

--- Shared/LockFree/Mutex.h
+++ Shared/LockFree/Mutex.h
@@ -13,6 +13,9 @@
 // number of spins
 #define DEFAULT_SPIN_COUNT 512
 
+// flag to use x64 BTS assembly in GCC/Clang (12~20% faster than XCHG!)
+#define USE_GCC_ASSEMBLY_X64_BTS (__x86_64__ && __GNUC__)
+
 // scoped lock
 template<typename mutex_t>
 class scoped_lock {
@@ -30,19 +33,46 @@ public:
     spinlock_mutex(int new_spin_count = DEFAULT_SPIN_COUNT) : spin_count(new_spin_count) {}
     
     inline void lock() {
+#if USE_GCC_ASSEMBLY_X64_BTS
+        mutex_try:
+        uint8_t ret;
+        asm (
+            "lock btsl $0, %0\n\t"
+            "setb %1\n\t"
+            : "+m" (flag), "=r" (ret)
+        );
+        
+        if(ret) {
+#else
         while(flag.test_and_set(std::memory_order_seq_cst)) {
+#endif
             for(int i = 0; i < spin_count; i++) {
                 std::this_thread::yield();
             }
+        
+#if USE_GCC_ASSEMBLY_X64_BTS
+            goto mutex_try;
+#endif
         }
     }
     
     inline void unlock() {
+#if USE_GCC_ASSEMBLY_X64_BTS
+        asm (
+            "lock btrl $0, %0\n\t"
+            : "+m" (flag)
+        );
+#else
         flag.clear(std::memory_order_seq_cst);
+#endif
     }
     
 private:
+#if USE_GCC_ASSEMBLY_X64_BTS
+    alignas(64) volatile int flag = 0;
+#else
     alignas(64) std::atomic_flag flag = ATOMIC_FLAG_INIT;
+#endif
     alignas(64) int spin_count;
 };
 
